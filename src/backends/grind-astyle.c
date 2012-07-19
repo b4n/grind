e780770fa2c1f00e16337f9fc83a2ab5a5c7b32e
Colomban Wendling <ban@herbesfolles.org>
2012-07-19 19:41:55 +0200

    Add options to the AStyle backend

--- src/backends/grind-astyle.c
+++ src/backends/grind-astyle.c
@@ -33,8 +33,30 @@
 
 #include "geanyplugin.h"
 
+#include "grind-enum-types.h"
 #include "grind-plugin.h"
 #include "grind-indenter.h"
+#include "grind-indenter-option.h"
+
+
+struct _GrindBackendAStylePrivate
+{
+  gint      style;
+  gint      indent_type;
+  gint      indent_width;
+  gint      brackets;
+  gboolean  indent_classes;
+  gboolean  indent_switches;
+  gboolean  indent_cases;
+  gboolean  indent_brackets;
+  gboolean  indent_blocks;
+  gboolean  indent_namespaces;
+  gboolean  indent_labels;
+  gboolean  indent_preprocessor;
+  gboolean  indent_col1_comments;
+  gint      max_instatement_indent;
+  gint      min_conditional_indent;
+};
 
 
 static const gchar *grind_backend_astyle_real_get_author      (GrindIndenter *base);
@@ -53,18 +75,215 @@ G_DEFINE_TYPE (GrindBackendAStyle, grind_backend_astyle, GRIND_TYPE_INDENTER)
 static void
 grind_backend_astyle_class_init (GrindBackendAStyleClass *klass)
 {
-  GrindIndenterClass *indenter_class = GRIND_INDENTER_CLASS (klass);
+  GObjectClass       *object_class    = G_OBJECT_CLASS (klass);
+  GrindIndenterClass *indenter_class  = GRIND_INDENTER_CLASS (klass);
+  guint               id = 0;
+  
+  object_class->get_property = grind_indenter_option_get_property_impl;
+  object_class->set_property = grind_indenter_option_set_property_impl;
   
   indenter_class->get_author      = grind_backend_astyle_real_get_author;
   indenter_class->get_description = grind_backend_astyle_real_get_description;
   indenter_class->get_name        = grind_backend_astyle_real_get_name;
   indenter_class->get_version     = grind_backend_astyle_real_get_version;
   indenter_class->indent          = grind_backend_astyle_real_indent;
+  
+  g_object_class_install_property (object_class, ++id,
+    grind_enum_indenter_option_new ("style",
+                                    "Style",
+                                    "Predefined styles",
+                                    GRIND_TYPE_BACKEND_ASTYLE_STYLE,
+                                    GRIND_BACKEND_ASTYLE_STYLE_ANSI,
+                                    G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                    G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                     style)));
+  g_object_class_install_property (object_class, ++id,
+    grind_enum_indenter_option_new ("indent-type",
+                                    "Indentation type",
+                                    "The type of the indentation. "
+                                    "\"dcoument setting\" uses the document's indentation type",
+                                    GRIND_TYPE_BACKEND_ASTYLE_INDENT_TYPE,
+                                    GRIND_BACKEND_ASTYLE_INDENT_TYPE_DOCUMENT_SETTING,
+                                    G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                    G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                     indent_type)));
+  g_object_class_install_property (object_class, ++id,
+    grind_int_indenter_option_new ("indent-width",
+                                   "Indentation width",
+                                   "The width of the indentation, in spaces. "
+                                   "A value of 0 uses the document's indentation width",
+                                   0,
+                                   20,
+                                   0,
+                                   G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                   G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                    indent_width)));
+  g_object_class_install_property (object_class, ++id,
+    grind_enum_indenter_option_new ("brackets",
+                                    "Brackets",
+                                    "Brackets breaking style",
+                                    GRIND_TYPE_BACKEND_ASTYLE_BRACKETS,
+                                    GRIND_BACKEND_ASTYLE_BRACKETS_BREAK,
+                                    G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                    G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                     brackets)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-classes",
+                                       "Indent classes",
+                                       "Indent 'class' blocks, so that the "
+                                       "inner 'public:', 'protected:' and "
+                                       "'private:' headers are indented in "
+                                       "relation to the class block. "
+                                       "This option has no effect on Java and "
+                                       "C# files.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_classes)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-switches",
+                                       "Indent switches",
+                                       "Indent 'switch' blocks, so that the "
+                                       "inner 'case X:' headers are indented "
+                                       "in relation to the switch block. "
+                                       "The entire case block is indented.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_switches)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-cases",
+                                       "Indent cases",
+                                       "Indent 'case X:' lines, so that they "
+                                       "are flush with their bodies. Case "
+                                       "statements not enclosed in blocks are "
+                                       "NOT indented.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_cases)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-brackets",
+                                       "Indent brackets",
+                                       "Add extra indentation to '{' and '}' "
+                                       "block brackets. This is the option "
+                                       "used for Whitesmith and Banner style "
+                                       "for‐matting/indenting. "
+                                       "If both indent-brackets and "
+                                       "indent-blocks are used the result will "
+                                       "be indent-blocks.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_brackets)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-blocks",
+                                       "Indent blocks",
+                                       "Add extra indentation to blocks within "
+                                       "a function. The opening bracket for "
+                                       "namespaces, classes, and functions is "
+                                       "not indented. This is the option used "
+                                       "for GNU style formatting/indenting.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_blocks)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-namespaces",
+                                       "Indent namespaces",
+                                       "Add extra indentation to namespace "
+                                       "blocks. This option has no effect on "
+                                       "Java files.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_namespaces)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-labels",
+                                       "Indent labels",
+                                       "Indent labels so that they appear one "
+                                       "indent less than the current "
+                                       "indentation level, rather than being "
+                                       "flushed completely to the left.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_labels)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-preprocessor",
+                                       "Indent preprocessor",
+                                       "Indent multi-line preprocessor "
+                                       "definitions ending with a backslash. "
+                                       "Should be used with convert-tabs for "
+                                       "proper results. Does a pretty good "
+                                       "job, but can not perform miracles in "
+                                       "obfuscated preprocessor definitions.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_preprocessor)));
+  g_object_class_install_property (object_class, ++id,
+    grind_boolean_indenter_option_new ("indent-col1-comments",
+                                       "Indent first column comments",
+                                       "Indent C++ comments  beginning in "
+                                       "column one. By default C++ comments "
+                                       "beginning in column one are not "
+                                       "indented. This option will allow the "
+                                       "comments to be indented with the code.",
+                                       FALSE,
+                                       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                       G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                        indent_col1_comments)));
+  g_object_class_install_property (object_class, ++id,
+    grind_int_indenter_option_new ("max-instatement-indent",
+                                   "Max in-statement indent",
+                                   "Indent a maximum of N spaces in a "
+                                   "continuous statement, relative to the "
+                                   "previous line.",
+                                   0,
+                                   80,
+                                   40,
+                                   G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                   G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                    max_instatement_indent)));
+  /* whatever AStyle's manual page or --help says, this is a FLAG that chooses
+   * between 0/0.5/1/2 times the current indent */
+  g_object_class_install_property (object_class, ++id,
+    grind_enum_indenter_option_new ("min-conditional-indent",
+                                    "Min conditional indent",
+                                    "Indent a minimal N*indent spaces in a"
+                                    "continuous conditional belonging to a"
+                                    "conditional header.",
+                                    GRIND_TYPE_BACKEND_ASTYLE_MIN_CONDITIONAL_MODE,
+                                    GRIND_BACKEND_ASTYLE_MIN_CONDITIONAL_MODE_TWO,
+                                    G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS,
+                                    G_STRUCT_OFFSET (GrindBackendAStylePrivate,
+                                                     min_conditional_indent)));
+  
+  g_type_class_add_private (klass, sizeof (GrindBackendAStylePrivate));
 }
 
 static void grind_backend_astyle_init (GrindBackendAStyle *self)
 {
+  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+                                            GRIND_TYPE_BACKEND_ASTYLE,
+                                            GrindBackendAStylePrivate);
   
+  self->priv->style = GRIND_BACKEND_ASTYLE_STYLE_ANSI;
+  self->priv->indent_type = GRIND_BACKEND_ASTYLE_INDENT_TYPE_DOCUMENT_SETTING;
+  self->priv->indent_width = 0;
+  self->priv->brackets = GRIND_BACKEND_ASTYLE_BRACKETS_BREAK;
+  self->priv->indent_classes = FALSE;
+  self->priv->indent_switches = FALSE;
+  self->priv->indent_cases = FALSE;
+  self->priv->indent_brackets = FALSE;
+  self->priv->indent_blocks = FALSE;
+  self->priv->indent_namespaces = FALSE;
+  self->priv->indent_labels = FALSE;
+  self->priv->indent_preprocessor = FALSE;
+  self->priv->indent_col1_comments = FALSE;
+  self->priv->max_instatement_indent = 40;
+  self->priv->min_conditional_indent = GRIND_BACKEND_ASTYLE_MIN_CONDITIONAL_MODE_TWO;
 }
 
 
@@ -141,6 +360,20 @@ build_args_add_file_type (GrindBackendAStyle *self,
   }
 }
 
+static void
+build_args_add_brackets_type (GrindBackendAStyle *self,
+                              GPtrArray          *array)
+{
+  GEnumClass *enum_class;
+  GEnumValue *enum_value;
+  
+  enum_class = g_type_class_ref (GRIND_TYPE_BACKEND_ASTYLE_BRACKETS);
+  enum_value = g_enum_get_value (enum_class, self->priv->brackets);
+  g_ptr_array_add (array, g_strdup_printf ("--brackets=%s",
+                                           enum_value->value_nick));
+  g_type_class_unref (enum_class);
+}
+
 static gchar **
 build_args (GrindBackendAStyle *self,
             GeanyDocument      *doc,
@@ -156,6 +389,40 @@ build_args (GrindBackendAStyle *self,
   build_args_add_indent_type (self, doc, array);
   build_args_add_eol_type (self, doc, array);
   build_args_add_file_type (self, doc, array);
+  build_args_add_brackets_type (self, array);
+  
+  if (self->priv->indent_classes) {
+    g_ptr_array_add (array, g_strdup ("--indent-classes"));
+  }
+  if (self->priv->indent_switches) {
+    g_ptr_array_add (array, g_strdup ("--indent-switches"));
+  }
+  if (self->priv->indent_cases) {
+    g_ptr_array_add (array, g_strdup ("--indent-cases"));
+  }
+  if (self->priv->indent_brackets) {
+    g_ptr_array_add (array, g_strdup ("--indent-brackets"));
+  }
+  if (self->priv->indent_blocks) {
+    g_ptr_array_add (array, g_strdup ("--indent-blocks"));
+  }
+  if (self->priv->indent_namespaces) {
+    g_ptr_array_add (array, g_strdup ("--indent-namespaces"));
+  }
+  if (self->priv->indent_labels) {
+    g_ptr_array_add (array, g_strdup ("--indent-labels"));
+  }
+  if (self->priv->indent_preprocessor) {
+    g_ptr_array_add (array, g_strdup ("--indent-preprocessor"));
+  }
+  if (self->priv->indent_col1_comments) {
+    g_ptr_array_add (array, g_strdup ("--indent-col1-comments"));
+  }
+  g_ptr_array_add (array, g_strdup_printf ("--max-instatement-indent=%d",
+                                           self->priv->max_instatement_indent));
+  /* although this is a flag it's passed as a digit */
+  g_ptr_array_add (array, g_strdup_printf ("--min-conditional-indent=%d",
+                                           self->priv->min_conditional_indent));
   
   g_ptr_array_add (array, g_strdup (filename)); /* process filename */
   g_ptr_array_add (array, NULL);
